881fb5115065e72af8304b9e706bd812daf9fd65
cdewangan24 <107066897+cdewangan24@users.noreply.github.com>
2026-01-24 22:09:27 -0600

    update on form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header () {
     return (
         <header>
-            <img className="header-chef-icon" src="../public/images/chef-claude-icon.png" alt="chef icon" />
+            <img className="header-chef-icon" src="../images/chef-claude-icon.png" alt="chef icon" />
             <h1 className="header-text">Chef Claude</h1>
         </header>
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,32 @@
+import React from "react"
+
 export default function Main () {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(formData) {
+        const newingredient = formData.get("ingredient");
+        setIngredients(prevIngredients => [...prevIngredients, newingredient]);
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button className="add-ingredient-button">+ Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
